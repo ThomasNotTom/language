39abb8a9fa2d89984fce381257bd739cec1804c7
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2026-05-17 21:33:10 +0100

    Use token class

--- src/lexer/tokens/primitives/primitive.cpp
+++ src/lexer/tokens/primitives/primitive.cpp
@@ -1,5 +1,5 @@
 #include "./primitive.hpp"
 #include "./primitive_type.hpp"
 
-Primitive::Primitive(const PrimitiveType primitiveType)
+PrimitiveToken::PrimitiveToken(const PrimitiveType primitiveType)
     : Token(TokenType::PRIMITIVE), primitiveType(primitiveType) {}

4eb9a03514c4e3d5f7eee28024af169b29e142fa
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2026-05-17 12:21:52 +0100

    Create read method

--- src/io/file_reader.cpp
+++ src/io/file_reader.cpp
@@ -1,9 +1,22 @@
 #include "file_reader.hpp"
 
 #include <fstream>
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
 
 FileReader::FileReader(const std::string filepath) : filepath(filepath) {
   this->inputStream = std::ifstream(filepath);
+  if (!this->inputStream) {
+    throw std::runtime_error("Failed to open file");
+  }
+};
+
+std::string FileReader::readAll() {
+  std::stringstream out;
+  out << this->inputStream.rdbuf();
+  this->inputStream.seekg(0);
+  return out.str();
 };
 
 char FileReader::readCharacter() {

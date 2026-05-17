58899b7b78cb148d2ba63df6040f3c7e85f44b62
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2026-05-17 21:29:31 +0100

    Include AST parser

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,17 @@
 #include "./io/file_reader.hpp"
 #include "./lexer/lexer.hpp"
 #include "./lexer/token_container/token_container.hpp"
+#include "syntax_analyser/abstract_syntax_tree.hpp"
 
 int main() {
   FileReader fileReader("./examples/variable_declaration.lang");
 
   Lexer lexer(fileReader.readAll());
   TokenContainer tokens = lexer.makeTokenList();
-
   tokens.print();
+
+  AbstractSyntaxTree ast(tokens);
+
+  Program program = ast.parse();
+  program.print();
 }

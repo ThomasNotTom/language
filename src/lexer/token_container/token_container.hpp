05c088f42d9375479ee69fee078f9e4869b98de3
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2026-05-17 21:30:44 +0100

    Create individual add methods

--- src/lexer/token_container/token_container.hpp
+++ src/lexer/token_container/token_container.hpp
@@ -3,7 +3,13 @@
 #include <memory>
 #include <vector>
 
-#include "../../lexer/tokens/token.hpp"
+#include "lexer/tokens/end_of_line/end_of_line.hpp"
+#include "lexer/tokens/identifier/identifier.hpp"
+#include "lexer/tokens/number/number.hpp"
+#include "lexer/tokens/operators/addition/addition.hpp"
+#include "lexer/tokens/operators/assignment/assignment.hpp"
+#include "lexer/tokens/primitives/uint8/uint8.hpp"
+#include "lexer/tokens/token.hpp"
 
 class TokenContainer {
 private:
@@ -12,8 +18,28 @@ private:
 public:
   TokenContainer();
 
-  void addToken(std::unique_ptr<Token> token) {
-    tokens.push_back(std::move(token));
+  void addEndOfLine(const EndOfLineToken& token) {
+    tokens.push_back(std::make_unique<EndOfLineToken>(token));
+  }
+
+  void addIdentifier(const IdentifierToken& token) {
+    tokens.push_back(std::make_unique<IdentifierToken>(token));
+  }
+
+  void addNumber(const NumberToken& token) {
+    tokens.push_back(std::make_unique<NumberToken>(token));
+  }
+
+  void addAddition(const AdditionToken& token) {
+    tokens.push_back(std::make_unique<AdditionToken>(token));
+  }
+
+  void addAssignment(const AssignmentToken& token) {
+    tokens.push_back(std::make_unique<AssignmentToken>(token));
+  }
+
+  void addUint8(const Uint8Token& token) {
+    tokens.push_back(std::make_unique<Uint8Token>(token));
   }
 
   Token* get(size_t index) const;

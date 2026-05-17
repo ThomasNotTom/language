5c05e07a6a96e924ef8dfbc16fa9b0d040bf61fa
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2026-05-17 12:23:11 +0100

    Create token container

---
+++ src/lexer/token_container/token_container.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+
+#include "../tokens/identifier/identifier.hpp"
+#include "../tokens/operators/operator.hpp"
+#include "../tokens/token_type.hpp"
+#include "./token_container.hpp"
+
+#include "../tokens/number/number.hpp"
+#include "../tokens/primitives/primitive.hpp"
+
+TokenContainer::TokenContainer() {};
+
+Token* TokenContainer::get(size_t index) const {
+  return this->tokens[index].get();
+}
+
+size_t TokenContainer::getCount() const { return this->tokens.size(); }
+
+void TokenContainer::print() const {
+  for (size_t i = 0; i < this->getCount(); i++) {
+    Token* token = this->get(i);
+
+    switch (token->getType()) {
+      case END_OF_LINE: {
+        std::cout << "END_OF_LINE\n";
+        break;
+      }
+
+      case PRIMITIVE: {
+        Primitive* primitive = (Primitive*)token;
+
+        switch (primitive->primitiveType) {
+          case UINT8: {
+            std::cout << "PRIMITIVE(UINT8)\n";
+            break;
+          }
+        }
+        break;
+      }
+
+      case OPERATOR: {
+        Operator* op = (Operator*)token;
+
+        switch (op->operatorType) {
+          case ASSIGNMENT: {
+            std::cout << "OPERATOR(=)\n";
+            break;
+          }
+
+          case ADDITION: {
+            std::cout << "OPERATOR(+)\n";
+            break;
+          }
+        }
+        break;
+      }
+
+      case IDENTIFIER: {
+        Identifier* identifier = (Identifier*)token;
+
+        std::cout << "IDENTIFIER(\"" << identifier->name << "\")\n";
+        break;
+      }
+
+      case NUMBER: {
+        Number* number = (Number*)token;
+
+        std::cout << "NUMBER(" << number->value << ")\n";
+        break;
+      }
+    }
+  }
+}

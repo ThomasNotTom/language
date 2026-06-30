00a213309ffedf86912816eb21e4b101a8ca4015
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2026-06-30 16:02:03 +0100

    Generalise to statement value

--- src/syntax_analyser/statement/return/return.cpp
+++ src/syntax_analyser/statement/return/return.cpp
@@ -1,7 +1,6 @@
 #include "syntax_analyser/statement/return/return.hpp"
 #include "./return.hpp"
-#include "lexer/tokens/identifier/identifier.hpp"
-#include "syntax_analyser/statement/value/identifier/identifier.hpp"
+#include "syntax_analyser/statement/value/value.hpp"
 
-ReturnStatement::ReturnStatement(std::unique_ptr<IdentifierValue> identifier)
-    : Statement(StatementType::RETURN), identifier(std::move(identifier)) {}
+ReturnStatement::ReturnStatement(std::unique_ptr<StatementValue> value)
+    : Statement(StatementType::RETURN), value(std::move(value)) {}

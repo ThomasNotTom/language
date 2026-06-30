110965d4832e9dfba94547b5481b5aad7c553f0d
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2026-06-30 15:26:14 +0100

    Create store method

--- src/generation/builder/builder.hpp
+++ src/generation/builder/builder.hpp
@@ -1,5 +1,8 @@
+#pragma once
+
 #include "llvm/IR/BasicBlock.h"
 #include "llvm/IR/Constants.h"
+#include "llvm/IR/DerivedTypes.h"
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/Instruction.h"
 #include "llvm/IR/Instructions.h"
@@ -20,19 +23,37 @@ public:
     this->irBuilder.SetInsertPoint(i);
   }
 
+  llvm::IntegerType* getUint8() const { return llvm::Type::getInt8Ty(context); }
+
   llvm::ConstantInt* createConst8(uint8_t value) {
-    return llvm::ConstantInt::get(llvm::Type::getInt8Ty(context), value);
+    return llvm::ConstantInt::get(this->getUint8(), value);
   };
+
+  llvm::IntegerType* getUint16() const {
+    return llvm::Type::getInt16Ty(context);
+  }
 
   llvm::ConstantInt* createConst16(uint16_t value) {
-    return llvm::ConstantInt::get(llvm::Type::getInt16Ty(context), value);
+    return llvm::ConstantInt::get(this->getUint16(), value);
   };
 
+  llvm::IntegerType* getUint32() const {
+    return llvm::Type::getInt32Ty(context);
+  }
+
   llvm::ConstantInt* createConst32(uint32_t value) {
-    return llvm::ConstantInt::get(llvm::Type::getInt32Ty(context), value);
+    return llvm::ConstantInt::get(this->getUint32(), value);
   };
 
   llvm::ReturnInst* createReturn(llvm::Value* value) {
     return this->irBuilder.CreateRet(value);
+  };
+
+  llvm::AllocaInst* allocate(llvm::Type* type, std::string name) {
+    return this->irBuilder.CreateAlloca(type, nullptr, name);
+  };
+
+  llvm::StoreInst* store(llvm::Value* value, llvm::Value* out) {
+    return this->irBuilder.CreateStore(value, out);
   };
 };

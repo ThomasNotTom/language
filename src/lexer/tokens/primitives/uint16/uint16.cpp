6ead6a5ebfc0223cfad1460884aaceaaaa99ffd9
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2026-07-31 00:59:06 +0100

    Create uint16 token

---
+++ src/lexer/tokens/primitives/uint16/uint16.cpp
@@ -0,0 +1,3 @@
+#include "./uint16.hpp"
+
+Uint16Token::Uint16Token() : PrimitiveToken(PrimitiveType::UINT16) {}
---
+++ src/lexer/tokens/primitives/uint16/uint16.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "../primitive.hpp"
+
+class Uint16Token : public PrimitiveToken {
+
+public:
+  Uint16Token();
+};

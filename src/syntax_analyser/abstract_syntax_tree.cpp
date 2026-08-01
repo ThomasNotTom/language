83beeec9e6862601e8742bac8b9e8876c1e78856
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2026-08-01 04:07:06 +0100

    Add uint64 to conversion

--- src/syntax_analyser/abstract_syntax_tree.cpp
+++ src/syntax_analyser/abstract_syntax_tree.cpp
@@ -17,7 +17,7 @@
 #include "syntax_analyser/statement/statement.hpp"
 #include "syntax_analyser/statement/value/identifier/identifier.hpp"
 #include "syntax_analyser/statement/value/number/number.hpp"
-#include "syntax_analyser/statement/value/value.hpp"
+
 #include <cstddef>
 #include <format>
 #include <memory>
@@ -37,6 +37,14 @@ AbstractSyntaxTree::getStatementPrimitiveTypeFromPrimitiveType(
 
     case PrimitiveType::UINT16: {
       return StatementPrimitiveType::UINT16;
+    }
+
+    case PrimitiveType::UINT32: {
+      return StatementPrimitiveType::UINT32;
+    }
+
+    case PrimitiveType::UINT64: {
+      return StatementPrimitiveType::UINT64;
     }
   }
 }

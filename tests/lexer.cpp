f54ec6bb9dfe45850f433f1282e6e65e1fa5d8cc
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2026-08-02 09:31:10 +0100

    Create test for singular variable declaration

--- tests/lexer.cpp
+++ tests/lexer.cpp
@@ -3,12 +3,30 @@
 #include "lexer/tokens/token_type.hpp"
 #include <catch2/catch_test_macros.hpp>
 
-TEST_CASE("Complex get with no arguments to constructor", "[complex]") {
-  Lexer lexer = Lexer("a;");
+TEST_CASE("Empty program", "[lexer]") {
+  Lexer lexer = Lexer("");
 
   TokenContainer tokenContainer = lexer.makeTokenList();
 
-  REQUIRE(tokenContainer.getCount() == 2);
-  REQUIRE(tokenContainer.view(0).tokenType == TokenType::IDENTIFIER);
-  REQUIRE(tokenContainer.view(1).tokenType == TokenType::END_OF_LINE);
+  REQUIRE(tokenContainer.getCount() == 0);
+};
+
+TEST_CASE("Empty line", "[lexer]") {
+  Lexer lexer = Lexer(";");
+
+  TokenContainer tokenContainer = lexer.makeTokenList();
+
+  REQUIRE(tokenContainer.getCount() == 1);
+  REQUIRE(tokenContainer.view(0).tokenType == TokenType::END_OF_LINE);
+};
+
+TEST_CASE("Singular variable declaration", "[lexer]") {
+  Lexer lexer = Lexer("uint8 a;");
+
+  TokenContainer tokenContainer = lexer.makeTokenList();
+
+  REQUIRE(tokenContainer.getCount() == 3);
+  REQUIRE(tokenContainer.view(0).tokenType == TokenType::PRIMITIVE);
+  REQUIRE(tokenContainer.view(1).tokenType == TokenType::IDENTIFIER);
+  REQUIRE(tokenContainer.view(2).tokenType == TokenType::END_OF_LINE);
 };

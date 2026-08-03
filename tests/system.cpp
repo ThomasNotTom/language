1a4e067adef633df39b3e59b046582182793ef72
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2026-08-04 00:01:57 +0100

    Create system tests on example files

--- tests/system.cpp
+++ tests/system.cpp
@@ -2,23 +2,78 @@
 
 #include "generation/generator.hpp"
 #include "io/file_reader.hpp"
-#include "lexer/lexer.hpp"
-#include "lexer/token_container/token_container.hpp"
-#include "syntax_analyser/abstract_syntax_tree.hpp"
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/internal/catch_test_failure_exception.hpp>
+#include <catch2/reporters/catch_reporter_event_listener.hpp>
+#include <catch2/reporters/catch_reporter_registrars.hpp>
+#include <cstdlib>
+#include <iostream>
+
+class CompilerBuildListener : public Catch::EventListenerBase {
+public:
+  using Catch::EventListenerBase::EventListenerBase;
+
+  void testRunStarting(const Catch::TestRunInfo& testRunInfo) override {
+    std::cout << "Building compiler\n";
+
+    int result = std::system("cmake . && make && ./bin/main");
+  }
+};
+
+CATCH_REGISTER_LISTENER(CompilerBuildListener)
+
+std::string exec(std::string cmd) {
+  std::array<char, 128> buffer;
+  std::string result;
+
+  std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd.c_str(), "r"),
+                                                pclose);
+
+  while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+    result += buffer.data();
+  }
+  return result;
+}
+
+void checkOutput(std::string filePath, std::string expectedOut) {}
 
 TEST_CASE("Run \"./examples/addition.lang\"", "[system]") {
-  FileReader fileReader("./examples/addition.lang");
-  Lexer lexer = Lexer(fileReader.readAll());
+  std::system("./bin/main ./examples/addition.lang");
+  int out = std::system("./main.out");
+  REQUIRE(out != -1);
+  REQUIRE(WIFEXITED(out));
+  REQUIRE(WEXITSTATUS(out) == 5);
+};
 
-  TokenContainer tokens = lexer.makeTokenList();
-  tokens.print();
+TEST_CASE("Run \"./examples/integer_primitives.lang\"", "[system]") {
+  std::system("./bin/main ./examples/integer_primitives.lang");
+  int out = std::system("./main.out");
+  REQUIRE(out != -1);
+  REQUIRE(WIFEXITED(out));
+  REQUIRE(WEXITSTATUS(out) == 1);
+};
 
-  AbstractSyntaxTree ast(tokens);
+TEST_CASE("Run \"./examples/print.lang\"", "[system]") {
+  std::system("./bin/main ./examples/print.lang");
+  std::string out = exec("./main.out");
 
-  Program program = ast.parse();
-  program.print();
+  REQUIRE(out == "1\n2\n");
+};
 
-  Generator generator(program);
-  generator.init();
-  generator.compile();
+TEST_CASE("Run \"./examples/transitive_assignment.lang\"", "[system]") {
+  std::system("./bin/main ./examples/transitive_assignment.lang");
+  int out = std::system("./main.out");
+
+  REQUIRE(out != -1);
+  REQUIRE(WIFEXITED(out));
+  REQUIRE(WEXITSTATUS(out) == 1);
+};
+
+TEST_CASE("Run \"./examples/variable_declaration.lang\"", "[system]") {
+  std::system("./bin/main ./examples/variable_declaration.lang");
+  int out = std::system("./main.out");
+
+  REQUIRE(out != -1);
+  REQUIRE(WIFEXITED(out));
+  REQUIRE(WEXITSTATUS(out) == 1);
 };

4503fe0b93bd82ba2e7169b706567a2ade7716bc
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2026-08-04 00:02:19 +0100

    Create verbose option

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,27 @@
 #include <llvm/Support/raw_ostream.h>
 
 #include <iostream>
+#include <memory>
 
 #include "./io/file_reader.hpp"
 #include "./lexer/lexer.hpp"
 #include "./lexer/token_container/token_container.hpp"
 #include "generation/generator.hpp"
+#include "generation/module.hpp"
 #include "syntax_analyser/abstract_syntax_tree.hpp"
 
 int main(const int argc, char* argv[]) {
   if (argc == 1) {
     std::cerr << "Provide an input file\n";
     return 1;
+  }
+
+  bool verbose = false;
+
+  if (argc == 3) {
+    if (std::string(argv[2]) == "-v") {
+      verbose = true;
+    }
   }
 
   FileReader fileReader(argv[1]);
@@ -25,14 +35,26 @@ int main(const int argc, char* argv[]) {
 
   Lexer lexer(fileReader.readAll());
   TokenContainer tokens = lexer.makeTokenList();
-  tokens.print();
+  if (verbose) {
+    tokens.print();
+  }
 
   AbstractSyntaxTree ast(tokens);
 
   Program program = ast.parse();
-  program.print();
+  if (verbose) {
+    program.print();
+  }
 
   Generator generator(program);
   generator.init();
-  generator.compile();
+
+  llvm::LLVMContext context;
+  std::unique_ptr<llvm::Module> module = generator.buildModule(context);
+
+  if (verbose) {
+    generator.print_module(std::move(module));
+  }
+
+  generator.compile(context, std::move(module));
 }

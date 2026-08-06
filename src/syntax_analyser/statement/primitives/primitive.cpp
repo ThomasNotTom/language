ffe91737514e146db2dc2904106dccc079e597fc
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2026-08-06 10:46:39 +0100

    Remove primitives from syntax analyser stage

--- src/syntax_analyser/statement/primitives/primitive.cpp
+++
@@ -1,5 +0,0 @@
-#include "./primitive.hpp"
-#include "./primitive_type.hpp"
-
-StatementPrimitive::StatementPrimitive(const StatementPrimitiveType primitiveType)
-    : primitiveType(primitiveType) {}
--- src/syntax_analyser/statement/primitives/primitive_type.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-enum class StatementPrimitiveType {
-  UINT8 = 1,
-  UINT16 = 2,
-  UINT32 = 3,
-  UINT64 = 4
-};

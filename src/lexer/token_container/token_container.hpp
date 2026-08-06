dbaf7f172e7acecad3189438beb6de9c863a7b9e
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2026-08-06 10:14:53 +0100

    Remove reference to primitives

--- src/lexer/token_container/token_container.hpp
+++ src/lexer/token_container/token_container.hpp
@@ -4,15 +4,10 @@
 #include <vector>
 
 #include "lexer/tokens/end_of_line/end_of_line.hpp"
-#include "lexer/tokens/identifier/identifier.hpp"
-#include "lexer/tokens/number/number.hpp"
 #include "lexer/tokens/operators/addition/addition.hpp"
 #include "lexer/tokens/operators/assignment/assignment.hpp"
 #include "lexer/tokens/operators/subtraction/subtraction.hpp"
-#include "lexer/tokens/primitives/uint16/uint16.hpp"
-#include "lexer/tokens/primitives/uint32/uint32.hpp"
-#include "lexer/tokens/primitives/uint64/uint64.hpp"
-#include "lexer/tokens/primitives/uint8/uint8.hpp"
+#include "lexer/tokens/other.hpp"
 #include "lexer/tokens/print/print.hpp"
 #include "lexer/tokens/return/return.hpp"
 #include "lexer/tokens/token.hpp"
@@ -28,12 +23,8 @@ public:
     tokens.push_back(std::make_unique<EndOfLineToken>(token));
   }
 
-  void addIdentifier(const IdentifierToken& token) {
-    tokens.push_back(std::make_unique<IdentifierToken>(token));
-  }
-
-  void addNumber(const NumberToken& token) {
-    tokens.push_back(std::make_unique<NumberToken>(token));
+  void addOther(const OtherToken& token) {
+    tokens.push_back(std::make_unique<OtherToken>(token));
   }
 
   void addAddition(const AdditionToken& token) {
@@ -46,22 +37,6 @@ public:
 
   void addAssignment(const AssignmentToken& token) {
     tokens.push_back(std::make_unique<AssignmentToken>(token));
-  }
-
-  void addUint8(const Uint8Token& token) {
-    tokens.push_back(std::make_unique<Uint8Token>(token));
-  }
-
-  void addUint16(const Uint16Token& token) {
-    tokens.push_back(std::make_unique<Uint16Token>(token));
-  }
-
-  void addUint32(const Uint32Token& token) {
-    tokens.push_back(std::make_unique<Uint32Token>(token));
-  }
-
-  void addUint64(const Uint64Token& token) {
-    tokens.push_back(std::make_unique<Uint64Token>(token));
   }
 
   void addReturn(const ReturnToken& token) {

90a2823eacc084cfc20dac77f95f8cdd34a4c6ec
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2026-08-06 10:47:31 +0100

    Move implementation into header file

--- src/syntax_analyser/statement/initialisation/initialisation.cpp
+++
@@ -1,8 +0,0 @@
-#include "./initialisation.hpp"
-#include "syntax_analyser/statement/primitives/primitive_type.hpp"
-#include "syntax_analyser/statement/value/identifier/identifier.hpp"
-
-InitialisationStatement::InitialisationStatement(
-    StatementPrimitiveType type, std::unique_ptr<IdentifierValue> identifier)
-    : Statement(StatementType::INITIALISATION), type(type),
-      identifier(std::move(identifier)) {}
--- src/syntax_analyser/statement/return/return.cpp
+++
@@ -1,6 +0,0 @@
-#include "syntax_analyser/statement/return/return.hpp"
-#include "./return.hpp"
-#include "syntax_analyser/statement/value/value.hpp"
-
-ReturnStatement::ReturnStatement(std::unique_ptr<StatementValue> value)
-    : Statement(StatementType::RETURN), value(std::move(value)) {}

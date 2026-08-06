61a2a0f515426a7c2732aa6f3eb4bb546b511d2e
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2026-08-06 10:47:12 +0100

    Remove value types

--- src/syntax_analyser/statement/assignment/assignment_type.hpp
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-enum class AssignmentType { NUMBER = 1, IDENTIFIER = 2 };
--- src/syntax_analyser/statement/assignment/identifier/identifier.cpp
+++
@@ -1,7 +0,0 @@
-#include "./identifier.hpp"
-#include "syntax_analyser/statement/value/identifier/identifier.hpp"
-
-AssignmentIdentifierStatement::AssignmentIdentifierStatement(
-    IdentifierValue identifier, IdentifierValue value)
-    : AssignmentStatement(AssignmentType::IDENTIFIER, identifier),
-      value(value) {};

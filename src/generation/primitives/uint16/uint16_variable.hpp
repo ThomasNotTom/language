b1b8081ad8b9f576984427a5ca2b5dcca9a60586
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2026-08-08 22:44:50 +0100

    Implement add method

--- src/generation/primitives/uint16/uint16_variable.hpp
+++ src/generation/primitives/uint16/uint16_variable.hpp
@@ -8,19 +8,36 @@ private:
   llvm::Type* llvmType;
 
 public:
-  Uint16Variable(Builder& builder) : llvmType(builder.getUint16()) {
+  Uint16Variable(Builder& builder, const BuilderType& builderType)
+      : Variable(builderType), llvmType(builder.getUint16()) {
     this->storage = builder.allocate(this->llvmType, "uint16");
   };
 
-  llvm::LoadInst* load(Builder& builder) {
+  llvm::LoadInst* load(Builder& builder) const {
     return builder.load(this->llvmType, this->storage, "uint16");
   }
 
-  llvm::StoreInst* store(Builder& builder, uint16_t other) {
+  llvm::StoreInst* store(Builder& builder, uint16_t other) const {
     return builder.store(builder.createConst16(other), this->storage);
   }
 
-  llvm::StoreInst* store(Builder& builder, Uint16Variable& other) {
+  llvm::StoreInst* store(Builder& builder, Uint16Variable& other) const {
     return builder.store(other.load(builder), this->storage);
+  }
+
+  void add(Builder& builder, const Variable& other) const override {
+    const unsigned int THIS_TYPE = this->getType();
+
+    if (other.getType() == THIS_TYPE) {
+      const Uint16Variable& uint16Other =
+          static_cast<const Uint16Variable&>(other);
+
+      builder.add(this->storage, uint16Other.load(builder),
+                  "uint16_add_uint16");
+    } else {
+      throw std::runtime_error("No addition method is definedb between type " +
+                               std::to_string(THIS_TYPE) + " and " +
+                               std::to_string(other.getType()));
+    }
   }
 };
--- src/generation/primitives/uint8/uint8_variable.hpp
+++ src/generation/primitives/uint8/uint8_variable.hpp
@@ -8,19 +8,35 @@ private:
   llvm::Type* llvmType;
 
 public:
-  Uint8Variable(Builder& builder) : llvmType(builder.getUint8()) {
+  Uint8Variable(Builder& builder, const BuilderType& builderType)
+      : Variable(builderType), llvmType(builder.getUint8()) {
     this->storage = builder.allocate(this->llvmType, "uint8");
   };
 
-  llvm::LoadInst* load(Builder& builder) {
+  llvm::LoadInst* load(Builder& builder) const {
     return builder.load(this->llvmType, this->storage, "uint8");
   }
 
-  llvm::StoreInst* store(Builder& builder, uint8_t other) {
+  llvm::StoreInst* store(Builder& builder, uint8_t other) const {
     return builder.store(builder.createConst8(other), this->storage);
   }
 
-  llvm::StoreInst* store(Builder& builder, Uint8Variable& other) {
+  llvm::StoreInst* store(Builder& builder, Uint8Variable& other) const {
     return builder.store(other.load(builder), this->storage);
-  }
+  };
+
+  void add(Builder& builder, const Variable& other) const override {
+    const unsigned int THIS_TYPE = this->getType();
+
+    if (other.getType() == THIS_TYPE) {
+      const Uint8Variable& uint8Other =
+          static_cast<const Uint8Variable&>(other);
+
+      builder.add(this->storage, uint8Other.load(builder), "uint8_add_uint8");
+    } else {
+      throw std::runtime_error("No addition method is definedb between type " +
+                               std::to_string(THIS_TYPE) + " and " +
+                               std::to_string(other.getType()));
+    };
+  };
 };

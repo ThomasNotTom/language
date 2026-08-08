a4f48408e83eaea3e8f31d39dbb48a55dee59147
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2026-08-08 22:48:37 +0100

    Define virtual methods

--- src/generation/variable.hpp
+++ src/generation/variable.hpp
@@ -1,12 +1,21 @@
 #pragma once
 
 #include "generation/builder/builder.hpp"
-#include "generation/type.hpp"
+// #include "generation/type.hpp"
+
+class BuilderType;
 
 class Variable {
 protected:
   llvm::Value* storage;
+  const BuilderType& builderType;
 
 public:
-  Variable() {};
+  Variable(const BuilderType& builderType) : builderType(builderType) {};
+
+  unsigned int getType() const;
+  virtual void add(Builder& builder, const Variable& other) const = 0;
+  virtual void returnValue(Builder& builder) const = 0;
+
+  virtual ~Variable() = default;
 };

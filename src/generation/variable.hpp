3b740e4abedea13cfb002e31a73ab8ed14c18a96
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2026-08-09 01:16:05 +0100

    Define variable methods

--- src/generation/variable.hpp
+++ src/generation/variable.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "generation/builder/builder.hpp"
-// #include "generation/type.hpp"
+#include "generation/callable/print.hpp"
 
 class BuilderType;
 
@@ -14,7 +14,19 @@ public:
   Variable(const BuilderType& builderType) : builderType(builderType) {};
 
   unsigned int getType() const;
+  virtual llvm::StoreInst* store(Builder& builder,
+                                 const Variable& other) const = 0;
+
+  virtual llvm::StoreInst* store(Builder& builder, int other) const = 0;
+
   virtual void add(Builder& builder, const Variable& other) const = 0;
+  virtual void add(Builder& builder, int other) const = 0;
+
+  virtual void subtract(Builder& builder, const Variable& other) const = 0;
+  virtual void subtract(Builder& builder, int other) const = 0;
+
+  virtual void print(Builder& builder,
+                     PrintCallableBuilder& printCallableBuilder) const = 0;
   virtual void returnValue(Builder& builder) const = 0;
 
   virtual ~Variable() = default;

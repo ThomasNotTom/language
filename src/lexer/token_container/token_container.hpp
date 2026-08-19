a32fbb76dbd5d6d7548da5ce6436f8acfae7a8ae
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2026-08-19 19:59:51 +0100

    Handle brackets

--- src/lexer/token_container/token_container.hpp
+++ src/lexer/token_container/token_container.hpp
@@ -3,13 +3,13 @@
 #include <memory>
 #include <vector>
 
+#include "lexer/tokens/bracket/bracket_close.hpp"
+#include "lexer/tokens/bracket/bracket_open.hpp"
 #include "lexer/tokens/end_of_line/end_of_line.hpp"
 #include "lexer/tokens/operators/addition/addition.hpp"
 #include "lexer/tokens/operators/assignment/assignment.hpp"
 #include "lexer/tokens/operators/subtraction/subtraction.hpp"
 #include "lexer/tokens/other.hpp"
-#include "lexer/tokens/print/print.hpp"
-#include "lexer/tokens/return/return.hpp"
 #include "lexer/tokens/token.hpp"
 
 class TokenContainer {
@@ -35,17 +35,19 @@ public:
     tokens.push_back(std::make_unique<SubtractionToken>(token));
   }
 
+  void addOpenBracket(const BracketOpen& token) {
+    tokens.push_back(std::make_unique<BracketOpen>(token));
+  }
+
+  void addCloseBracket(const BracketClose& token) {
+    tokens.push_back(std::make_unique<BracketClose>(token));
+  }
+
   void addAssignment(const AssignmentToken& token) {
     tokens.push_back(std::make_unique<AssignmentToken>(token));
   }
 
-  void addReturn(const ReturnToken& token) {
-    tokens.push_back(std::make_unique<ReturnToken>(token));
-  }
-
-  void addPrint(const PrintToken& token) {
-    tokens.push_back(std::make_unique<PrintToken>(token));
-  }
+  static std::string tokenMetadataToString(const TokenMetadata& metadata);
 
   const Token& view(size_t index) const;
   size_t getCount() const;

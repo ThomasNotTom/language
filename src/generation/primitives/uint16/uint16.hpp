a159e24ea6c8d30c570209b66e1f5b4cd865732f
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2026-08-19 19:41:48 +0100

    Use initialisation statement

--- src/generation/primitives/uint16/uint16.hpp
+++ src/generation/primitives/uint16/uint16.hpp
@@ -1,13 +1,17 @@
+#include <functional>
+
 #include "generation/primitives/uint16/uint16_variable.hpp"
 #include "generation/type.hpp"
-
-#include <functional>
+#include "syntax_analyser/statement/initialisation/initialisation.hpp"
 
 class Uint16Builder : public BuilderType {
 public:
   Uint16Builder(unsigned int typeID) : BuilderType(typeID) {};
 
-  std::unique_ptr<Variable> makeVariable(Builder& builder) const override {
-    return std::make_unique<Uint16Variable>(builder, *this);
+  std::unique_ptr<Variable> makeVariable(
+      Builder& builder,
+      const InitialisationStatement& initialisationStatement) const override {
+    return std::make_unique<Uint16Variable>(builder, *this,
+                                            initialisationStatement);
   };
 };
--- src/generation/primitives/uint32/uint32.hpp
+++ src/generation/primitives/uint32/uint32.hpp
@@ -1,13 +1,17 @@
+#include <functional>
+
 #include "generation/primitives/uint32/uint32_variable.hpp"
 #include "generation/type.hpp"
-
-#include <functional>
+#include "syntax_analyser/statement/initialisation/initialisation.hpp"
 
 class Uint32Builder : public BuilderType {
 public:
   Uint32Builder(unsigned int typeID) : BuilderType(typeID) {};
 
-  std::unique_ptr<Variable> makeVariable(Builder& builder) const override {
-    return std::make_unique<Uint32Variable>(builder, *this);
+  std::unique_ptr<Variable> makeVariable(
+      Builder& builder,
+      const InitialisationStatement& initialisationStatement) const override {
+    return std::make_unique<Uint32Variable>(builder, *this,
+                                            initialisationStatement);
   };
 };

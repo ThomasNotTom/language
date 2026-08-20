14d92c951dbc12d89fa79be33d9763a3d94a3b1a
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2026-08-20 15:17:00 +0100

    Create is float method

--- src/lexer/matcher.hpp
+++ src/lexer/matcher.hpp
@@ -6,6 +6,10 @@
 class Matcher {
 public:
   static bool isInt(const std::string& string) {
-    return std::regex_match(string, std::regex("^-?[0-9]+$"));
+    return std::regex_match(string, std::regex("-?[0-9]+"));
   };
+
+  static bool isFloat(const std::string& string) {
+    return std::regex_match(string, std::regex("-?[0-9]+\\.[0-9]+"));
+  }
 };

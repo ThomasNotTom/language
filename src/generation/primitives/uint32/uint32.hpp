ff999060c952a46f2a0a138ab2eb20e2aa92d6dd
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2026-08-24 22:15:02 +0100

    Use builder type id

--- src/generation/primitives/uint32/uint32.hpp
+++ src/generation/primitives/uint32/uint32.hpp
@@ -3,10 +3,11 @@
 #include "generation/primitives/uint32/uint32_variable.hpp"
 #include "generation/type.hpp"
 #include "syntax_analyser/statement/initialisation/initialisation.hpp"
+#include "generation/primitives/builder_type.hpp"
 
 class Uint32Builder : public BuilderType {
 public:
-  Uint32Builder(unsigned int typeID) : BuilderType(typeID) {};
+  Uint32Builder() : BuilderType((uint8_t)BuilderTypeID::UINT32) {};
 
   std::unique_ptr<Variable> makeVariable(
       Builder& builder,
--- src/generation/primitives/uint64/uint64.hpp
+++ src/generation/primitives/uint64/uint64.hpp
@@ -1,12 +1,13 @@
 #include <functional>
 
+#include "generation/primitives/builder_type.hpp"
 #include "generation/primitives/uint64/uint64_variable.hpp"
 #include "generation/type.hpp"
 #include "syntax_analyser/statement/initialisation/initialisation.hpp"
 
 class Uint64Builder : public BuilderType {
 public:
-  Uint64Builder(unsigned int typeID) : BuilderType(typeID) {};
+  Uint64Builder() : BuilderType((uint8_t)BuilderTypeID::UINT64) {};
 
   std::unique_ptr<Variable> makeVariable(
       Builder& builder,

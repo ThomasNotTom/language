9e9efa842813814b89e83235ff74ce7b88746c93
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2026-08-24 13:19:56 +0100

    Create float converter

--- src/lexer/string_converter.hpp
+++ src/lexer/string_converter.hpp
@@ -1,10 +1,21 @@
 #pragma once
 
+#include <regex>
 #include <string>
 
 class StringConverter {
 public:
+  static bool isInt(const std::string& string) {
+    return std::regex_match(string, std::regex("-?[0-9]+"));
+  };
+
+  static bool isFloat(const std::string& string) {
+    return std::regex_match(string, std::regex("-?[0-9]+\\.[0-9]+"));
+  }
+
   static uint64_t toUnsignedLongLong(const std::string& string) {
     return std::stoull(string);
   }
+
+  static float toFloat(const std::string& string) { return std::stof(string); }
 };

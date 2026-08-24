6e82a0f1f2023c72f15d2862279fc6b6a68342ca
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2026-08-24 13:19:38 +0100

    Take in parameters

--- src/generation/callable/return.hpp
+++ src/generation/callable/return.hpp
@@ -2,24 +2,53 @@
 
 #include <llvm/IR/Module.h>
 #include <llvm/IR/Value.h>
+#include <memory>
+#include <stdexcept>
 
 #include "generation/builder/builder.hpp"
 #include "generation/callable/callable.hpp"
 #include "generation/variable.hpp"
+#include "lexer/string_converter.hpp"
 
 class ReturnCallableBuilder : public Callable {
 public:
   ReturnCallableBuilder() : Callable() {};
 
-  void call(Builder& builder, const Variable& out) override {
-    llvm::Value* extended =
-        builder.zext(out.load(builder), builder.getUint32());
-    builder.createReturn(extended);
-  }
+  void
+  call(Builder& builder,
+       const std::vector<std::unique_ptr<Parameter>>& parameters) override {
 
-  void call(Builder& builder, uint64_t out) override {
-    llvm::Value* extended =
-        builder.zext(builder.createConst64(out), builder.getUint32());
-    builder.createReturn(extended);
+    if (parameters.size() != 1) {
+      // TODO: Better error message
+      throw std::runtime_error("Print takes only one value");
+    }
+
+    const Parameter& parameter = *parameters[0];
+
+    if (parameter.getType() == ParamaterType::VALUE) {
+      const ParameterValue& parameterValue =
+          static_cast<const ParameterValue&>(parameter);
+
+      // TODO: Better error message
+      if (!StringConverter::isInt(parameterValue.getValue())) {
+        throw std::runtime_error("Cannot return non-int");
+      }
+      uint32_t value =
+          StringConverter::toUnsignedLongLong(parameterValue.getValue());
+
+      llvm::Value* extended =
+          builder.zext(builder.createConst32(value), builder.getUint32());
+      builder.createReturn(extended);
+      return;
+    }
+
+    else if (parameter.getType() == ParamaterType::VARIABLE) {
+      const ParameterVariable& parameterVariable =
+          static_cast<const ParameterVariable&>(parameter);
+
+      llvm::Value* extended = builder.zext(
+          parameterVariable.getVariable().load(builder), builder.getUint32());
+      builder.createReturn(extended);
+    }
   }
 };

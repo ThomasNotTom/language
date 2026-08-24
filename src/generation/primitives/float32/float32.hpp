c23c678e098b7f1d092d51e49856334191b331f9
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2026-08-24 22:14:41 +0100

    Use builder type id

--- src/generation/primitives/float32/float32.hpp
+++ src/generation/primitives/float32/float32.hpp
@@ -1,17 +1,18 @@
 
-#include "generation/primitives/builder_type.hpp"
-#include "generation/primitives/float16/float16_variable.hpp"
+#include "generation/primitives/float32/float32_variable.hpp"
 #include "generation/type.hpp"
 #include "syntax_analyser/statement/initialisation/initialisation.hpp"
 
-class Float16Builder : public BuilderType {
+#include "generation/primitives/builder_type.hpp"
+
+class Float32Builder : public BuilderType {
 public:
-  Float16Builder() : BuilderType((uint8_t)BuilderTypeID::FLOAT16) {};
+  Float32Builder() : BuilderType((uint8_t)BuilderTypeID::FLOAT32) {};
 
   std::unique_ptr<Variable> makeVariable(
       Builder& builder,
       const InitialisationStatement& initialisationStatement) const override {
-    return std::make_unique<Float16Variable>(builder, *this,
+    return std::make_unique<Float32Variable>(builder, *this,
                                              initialisationStatement);
   };
 };
--- src/generation/primitives/float64/float64.hpp
+++ src/generation/primitives/float64/float64.hpp
@@ -1,17 +1,19 @@
 
+#include <cstdint>
+
 #include "generation/primitives/builder_type.hpp"
-#include "generation/primitives/float16/float16_variable.hpp"
+#include "generation/primitives/float64/float64_variable.hpp"
 #include "generation/type.hpp"
 #include "syntax_analyser/statement/initialisation/initialisation.hpp"
 
-class Float16Builder : public BuilderType {
+class Float64Builder : public BuilderType {
 public:
-  Float16Builder() : BuilderType((uint8_t)BuilderTypeID::FLOAT16) {};
+  Float64Builder() : BuilderType((uint8_t)BuilderTypeID::FLOAT64) {};
 
   std::unique_ptr<Variable> makeVariable(
       Builder& builder,
       const InitialisationStatement& initialisationStatement) const override {
-    return std::make_unique<Float16Variable>(builder, *this,
+    return std::make_unique<Float64Variable>(builder, *this,
                                              initialisationStatement);
   };
 };

10c6e28f41b51c9ce8d73d6f07d20bf9c1759ee9
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2026-09-05 01:21:10 +0100

    Create is operator method

--- src/lexer/tokens/token_type.hpp
+++ src/lexer/tokens/token_type.hpp
@@ -1,11 +1,26 @@
 #pragma once
 
 #include <cstdint>
+#include <string>
 
 enum TokenType : uint8_t {
   END_OF_LINE = 1,
-  OPERATOR = 2,
-  OTHER = 3,
-  BRACKET_OPEN = 4,
-  BRACKET_CLOSE = 5
+  PLUS = 2,
+  MINUS = 3,
+  EQUALS = 4,
+  BRACKET_OPEN = 5,
+  BRACKET_CLOSE = 6,
+  OTHER = 7,
+};
+
+class TokenChecker {
+public:
+  static bool isOperator(const TokenType& tokenType) {
+    return tokenType == TokenType::PLUS || tokenType == TokenType::MINUS;
+  }
+
+  static std::string getOperatorTypes() {
+    return std::to_string(TokenType::PLUS) + ", " +
+           std::to_string(TokenType::MINUS);
+  }
 };

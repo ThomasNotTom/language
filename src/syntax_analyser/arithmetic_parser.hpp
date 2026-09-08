d9c48dbb829ba14228a78587b2394487f2a7a7d5
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2026-09-08 02:42:51 +0100

    Implement simple shunting-yard

--- src/syntax_analyser/arithmetic_parser.hpp
+++ src/syntax_analyser/arithmetic_parser.hpp
@@ -1,9 +1,14 @@
 #include <cstdint>
+#include <functional>
+#include <iostream>
 #include <map>
+#include <memory>
+#include <queue>
 #include <stack>
+#include <stdexcept>
+#include <string>
 #include <vector>
 
-#include "lexer/tokens/operators/operator.hpp"
 #include "lexer/tokens/other.hpp"
 #include "lexer/tokens/token.hpp"
 #include "lexer/tokens/token_type.hpp"
@@ -14,8 +19,6 @@ enum class TokenPriority : uint8_t {
   ADDITION = 2,
   SUBTRACTION = 3
 };
-
-// std::map<OperatorToken> a;
 
 class TokenSegment {
 private:
@@ -36,8 +39,32 @@ public:
 
 class ArithmeticParser {
 private:
+  static uint8_t getTokenPriority(std::reference_wrapper<const Token> token) {
+    switch (token.get().tokenType) {
+      case TokenType::PLUS:
+      case TokenType::MINUS: {
+        return 2;
+      }
+
+      default: {
+        throw std::runtime_error("Cannot get priority of token " +
+                                 std::to_string(token.get().tokenType));
+      }
+    }
+  };
+
+  static bool isOperator(const Token& token) {
+    switch (token.tokenType) {
+      case TokenType::PLUS:
+      case TokenType::MINUS:
+        return true;
+      default:
+        return false;
+    }
+  }
+
 public:
-  std::stack<TokenSegment>
+  static std::vector<std::unique_ptr<Statement>>
   parse(const std::vector<std::reference_wrapper<const Token>>& tokens,
         const OtherToken& outToken) {
     // 3 + 4 - 1 + 2; 8 -> 4
@@ -45,43 +72,99 @@ public:
     // 3 + 4
     // (3 + 4) - 1
 
-    std::stack<std::reference_wrapper<const Token>> inputStack =
-        std::stack<std::reference_wrapper<const Token>>();
-    std::stack<std::reference_wrapper<const Token>> outputStack =
-        std::stack<std::reference_wrapper<const Token>>();
+    std::queue<std::reference_wrapper<const Token>> outputQueue =
+        std::queue<std::reference_wrapper<const Token>>();
     std::stack<std::reference_wrapper<const Token>> operatorStack =
         std::stack<std::reference_wrapper<const Token>>();
 
+    // while there are tokens to be read:
     for (size_t i = 0; i < tokens.size(); i++) {
-      inputStack.push(tokens[i]);
+      const Token& token = tokens[i];
+      //     read a token
+
+      //     if the token is:
+      //     - a number:
+      if (token.tokenType == TokenType::OTHER) {
+        outputQueue.push(token);
+        //         put it into the output queue
+      }
+      //     - a function:
+      else if (false) {
+        //         push it onto the operator stack
+      }
+      //     - an operator o1:
+      else if (ArithmeticParser::isOperator(token)) {
+        const Token& o1 = token;
+        //         while (
+        while (true) {
+          std::cout << "a\n";
+          //             there is an operator o2 at the top of the operator
+          //             stack
+          if (operatorStack.empty()) {
+            break;
+          }
+          const Token& o2 = operatorStack.top();
+          //             which is not a left parenthesis,
+          if (o2.tokenType == TokenType::BRACKET_OPEN) {
+            break;
+          }
+
+          uint8_t o1Precedence = ArithmeticParser::getTokenPriority(o1);
+          uint8_t o2Precedence = ArithmeticParser::getTokenPriority(o2);
+
+          //             and (o2 has greater
+          //             precedence than o1 or (o1 and o2 have the same
+          //             precedence and o1 is left-associative))
+          //         ):
+          if (!(o2Precedence >= o1Precedence)) {
+            break;
+          }
+
+          //             pop o2 from the operator stack into the output queue
+          operatorStack.pop();
+          outputQueue.push(o2);
+        }
+        //         push o1 onto the operator stack
+        operatorStack.push(o1);
+      }
+      //     - a ",":
+      //         while the operator at the top of the operator stack is not a
+      //         left parenthesis:
+      //              pop the operator from the operator stack into the output
+      //              queue
+      //     - a left parenthesis (i.e. "("):
+      //         push it onto the operator stack
+      //     - a right parenthesis (i.e. ")"):
+      //         while the operator at the top of the operator stack is not a
+      //         left parenthesis:
+      //             {assert the operator stack is not empty}
+      //             /* If the stack runs out without finding a left
+      //             parenthesis, then there are mismatched parentheses. */
+      //             pop the operator from the operator stack into the output
+      //             queue
+      //         {assert there is a left parenthesis at the top of the
+      //         operator stack} pop the left parenthesis from the operator
+      //         stack and discard it if there is a function token at the top
+      //         of the operator stack, then:
+      //             pop the function from the operator stack into the output
+      //             queue
     }
+    // /* After the while loop, pop the remaining items from the operator
+    // stack into the output queue.
+    while (!operatorStack.empty()) {
+      const Token& operatorToken = operatorStack.top();
+      operatorStack.pop();
 
-    size_t i = 0;
+      outputQueue.push(operatorToken);
+    }
+    // */ while there are tokens on the operator stack:
+    //     /* If the operator token on the top of the stack is a parenthesis,
+    //     then there are mismatched parentheses. */ {assert the operator on
+    //     top of the stack is not a (left) parenthesis} pop the operator from
+    //     the operator stack onto the output queue
 
-    while (!inputStack.empty()) {
-      const Token& next = inputStack.top();
-      inputStack.pop();
+    std::vector<std::unique_ptr<Statement>> out;
 
-      if (next.tokenType == TokenType::OTHER) {
-        outputStack.push(next);
-        i += 1;
-        continue;
-      }
-
-      if (next.tokenType == TokenType::OPERATOR) {
-        const OperatorToken& nextOperator =
-            static_cast<const OperatorToken&>(next);
-        uint8_t priority;
-
-        if (nextOperator.tokenType ==TokenType::)
-          outputStack.push(next);
-        i += 1;
-        continue;
-      }
-    };
-
-    outputStack.push(tokens[0]);
-
-    return tokenStack;
+    return out;
   }
 };

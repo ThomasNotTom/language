1b3674a493f602727f26ca764cec95fd419b62d9
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2026-09-18 13:42:23 +0100

    Compile verbosely

--- tests/system.cpp
+++ tests/system.cpp
@@ -39,7 +39,7 @@ std::string exec(const std::string& cmd) {
 void checkOutput(const std::string& filePath, const std::string& expectedOut) {}
 
 TEST_CASE("Run \"./examples/addition.lang\"", "[system]") {
-  std::system("./bin/main ./examples/addition.lang");
+  std::system("./bin/main ./examples/addition.lang -v");
   int out = std::system("./main.out");
   REQUIRE(out != -1);
   REQUIRE(WIFEXITED(out));
@@ -62,7 +62,7 @@ TEST_CASE("Run \"./examples/print.lang\"", "[system]") {
 };
 
 TEST_CASE("Run \"./examples/subtraction.lang\"", "[system]") {
-  std::system("./bin/main ./examples/subtraction.lang");
+  std::system("./bin/main ./examples/subtraction.lang -v");
   int out = std::system("./main.out");
 
   REQUIRE(out != -1);

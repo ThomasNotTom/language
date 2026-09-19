0415b7085caea3b103763bed7a93bf68869aac71
Thomas <31802793+ThomasNotTom@users.noreply.github.com>
2026-09-19 04:03:14 +0100

    Define string to llvm value method

--- src/generation/variable.hpp
+++ src/generation/variable.hpp
@@ -29,6 +29,7 @@ public:
   virtual llvm::LoadInst* load(Builder& builder) const = 0;
 
   llvm::Value* getStorage() const { return this->storage; };
+  const BuilderType& getBuilderType() const { return this->builderType; };
 
   virtual llvm::StoreInst* store(Builder& builder,
                                  const Variable& other) const = 0;
@@ -52,6 +53,11 @@ public:
   const InitialisationStatement& getInit() const {
     return this->initialisationStatement;
   };
+
+  virtual llvm::Value* stringToLLVMValue(const Builder& builder,
+                                         const std::string& value) const {
+    return nullptr;
+  }
 
   virtual ~Variable() = default;
 };
